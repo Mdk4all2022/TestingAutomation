db6964e09ba16d431619aac1f3a54b839e05ead6
Marwan_Mac_Git <mdk4all2003@yahoo.com>
2023-05-10 19:57:52 -0500

    added Testing case 124

---
+++ src/utils/TestingCase124.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class TestingCase124 {
+    public static void main(String[] args) {
+        //start testing
+        // find bugs
+        //fix them
+        //do the report
+    }
+}
